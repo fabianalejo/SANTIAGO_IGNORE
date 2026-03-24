3bc007be10d21afeaac8cbb0e118dbda8f7b23e3
copito de nievesota <cruzycada@gmail.com>
2026-03-24 12:57:29 -0300

    commit con ignore

--- ej1.py
+++ ej1.py
@@ -5,7 +5,7 @@ n3 = int(input('Introduzca un numero mas '))
 if n1>n2 and n1>n3:
     print(f'El numero mas grande es {n1}')
 elif n2>n3:
-    print(f'El numero mas grande es {n2}')
+    print(f'El numero menos grande es {n2}')
 else:
     print(f'El numero mas grande es {n3}')
 
